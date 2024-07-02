c2a5c941603d25ae93495ce98377b255dcf42de1
martinaliporata <martinaliporata@gmail.com>
2024-07-02 16:52:47 +0200

    implementing method constructor

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 class Movie {
+    public $name;
+    public $year;
+    public $director;
+    public $bestActor;
 
+    function __construct($_bestActor) {
+        $this->bestActor = $_bestActor;
+    }
 };
+
+$gladiator = new Movie(Russel);
+echo $gladiator->bestActor;
 
 $gladiator = new Movie();
 $batman = new Movie();
 $birdman = new Movie();
+
+$gladiator -> name = "Gladiatore";
+$gladiator -> year = "2000";
+$gladiator -> director = "Ridley Scott";
