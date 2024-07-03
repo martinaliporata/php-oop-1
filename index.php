b4fc19716e9de643d44824e24235c4e76457dc45
martinaliporata <martinaliporata@gmail.com>
2024-07-03 10:40:14 +0200

    added comments

--- index.php
+++ index.php
@@ -8,24 +8,26 @@ class Movie {
     public $bestActor;
     public $ticketPrice;
 
-    // method
-    public function description() {
-        return "{$this -> name} is a {$this -> year} film directed by {$this -> director} and whose best actor is {$this -> bestActor} and it's now out in cinemas at the price of {$this -> ticketPrice}";
-    }
-
     // constructor
-    function __construct($movieName, $movieYear, $movieDirector, $movieBestActor, $movieTicketPrice) {
+    public function __construct($movieName, $movieYear, $movieDirector, $movieBestActor, $movieTicketPrice) {
         $this-> name = $movieName;
         $this-> year = $movieYear;
         $this-> director = $movieDirector;
         $this-> bestActor = $movieBestActor;
         $this-> ticketPrice = $movieTicketPrice;
-    }};
+        // public $name è $this-> name; __construct($movieName è = $movieName   
+    }
+
+    // method
+    public function getDescription() {
+        return "{$this -> name} is a {$this -> year} film directed by {$this -> director} and whose best actor is {$this -> bestActor} and it's now out in cinemas at the price of {$this -> ticketPrice}";
+    }
+
+    };
 
     // Questi sono gli oggetti movie
-
     $gladiator = new Movie("Gladiator", "2000", "Ridley Scott", "Russell Crowe", "15");
     $titanic = new Movie("Titanic", "1997", "James Cameron", "Leonardo Di Caprio", "8");
 
-    echo $gladiator -> description();
-    echo $titanic -> description();
+    echo $gladiator -> getDescription();
+    echo $titanic -> getDescription();
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
-è definita una classe ‘Movie’ OK
+è definita una classe ‘Movie’
+
 => all'interno della classe sono dichiarate delle variabili d'istanza
 
 => all'interno della classe è definito un costruttore
